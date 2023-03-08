4ced7dcddd5d3e4bbcfd0a11b95bce49020344f8
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2023-03-08 16:02:53 -0500

    added footer.js and imported the file in app.js to display on application

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,6 +1,14 @@
 import React from "react";
+import Footer from "./components/Footer";
 import PortfolioContainer from "./components/PortfolioContainer";
 
-const App = () => <PortfolioContainer />;
+function App() {
+  return (
+    <div>
+      <PortfolioContainer />
+      <Footer />
+    </div>
+  );
+}
 
 export default App;
---
+++ myportfolio/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      <footer>
+        <ul>
+          <li>Github</li>
+          <li>Linkedin</li>
+        </ul>
+      </footer>
+    </div>
+  );
+}
