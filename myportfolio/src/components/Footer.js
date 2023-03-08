629446c17eb67b9ddb32d80010a8499a0c47a611
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2023-03-08 16:19:24 -0500

    added babel files to include font awesome plugin

--- myportfolio/src/components/Footer.js
+++ myportfolio/src/components/Footer.js
@@ -1,13 +1,21 @@
 import React from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  solid,
+  regular,
+  brands,
+  icon,
+} from "@fortawesome/fontawesome-svg-core/import.macro";
+
 export default function Footer() {
   return (
     <div>
       <footer>
-        <ul>
-          <li>Github</li>
-          <li>Linkedin</li>
-        </ul>
+        <FontAwesomeIcon icon={solid("user-secret")} />
+        <FontAwesomeIcon icon={regular("coffee")} />
+        <FontAwesomeIcon icon={icon({ name: "coffee", style: "solid" })} />
+        <FontAwesomeIcon icon={brands("twitter")} />
       </footer>
     </div>
   );
