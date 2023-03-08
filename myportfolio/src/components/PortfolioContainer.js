72eaf4b1b3852f26f444aee1214bb06694485a15
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2023-03-08 15:25:49 -0500

    updated file names and layout

--- myportfolio/src/components/PortfolioContainer.js
+++ myportfolio/src/components/PortfolioContainer.js
@@ -1,20 +1,19 @@
 import React, { useState } from "react";
 import NavTabs from "./NavTabs";
-import Home from "./pages/Home";
-import About from "./pages/About";
+import AboutMe from "./pages/AboutMe";
+import Portfolio from "./pages/Portfolio";
 import Blog from "./pages/Blog";
 import Contact from "./pages/Contact";
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState("Home");
 
-  // TODO: Add a comment describing the functionality of this method
   const renderPage = () => {
-    if (currentPage === "Home") {
-      return <Home />;
+    if (currentPage === "AboutMe") {
+      return <AboutMe />;
     }
-    if (currentPage === "About") {
-      return <About />;
+    if (currentPage === "Portfolio") {
+      return <Portfolio />;
     }
     if (currentPage === "Blog") {
       return <Blog />;
@@ -26,9 +25,7 @@ export default function PortfolioContainer() {
 
   return (
     <div>
-      {/* // TODO: Add a comment describing what we are passing as props */}
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
-      {/* // TODO: Add a comment explaining what is happening on the following line */}
       {renderPage()}
     </div>
   );
