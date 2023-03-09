bbd569dad79d7d2d772bd057b148757922486ccc
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2023-03-09 11:43:02 -0500

    updated navbar with bootstrap className

--- myportfolio/src/components/NavTabs.js
+++ myportfolio/src/components/NavTabs.js
@@ -11,59 +11,65 @@ function NavTabs({ currentPage, handlePageChange }) {
           // Ternary operater is checking if the current page is the Home page, if it is on the blog it will make the nav-link active
           className={currentPage === "Home" ? "nav-link active" : "nav-link"}
         >
-          Caleb Carnett
+          {/* <img
+            src={require("../../src/styles/images/headshot.jpg")}
+            className="img-fluid mw-300"
+            alt="Responsive image"
+          /> */}
         </a>
       </div>
-      <ul className="nav nav-tabs">
-        <li className="nav-item">
-          <a
-            href="#AboutMe"
-            onClick={() => handlePageChange("AboutMe")}
-            // Ternary operater is checking if the current page is the About me page, if it is on the blog it will make the nav-link active
-            className={
-              currentPage === "AboutMe" ? "nav-link active" : "nav-link"
-            }
-          >
-            About Me
-          </a>
-        </li>
-        <li className="nav-item">
-          <a
-            href="#Portfolio"
-            onClick={() => handlePageChange("Portfolio")}
-            // Ternary operater is checking if the current page is the portfolio page, if it is on the blog it will make the nav-link active
-            className={
-              currentPage === "Portfolio" ? "nav-link active" : "nav-link"
-            }
-          >
-            Portfolio
-          </a>
-        </li>
-        <li className="nav-item">
-          <a
-            href="#contact"
-            // Ternary operater is checking if the current page is the the contact page, if it is on the blog it will make the nav-link active
-            onClick={() => handlePageChange("Contact")}
-            className={
-              currentPage === "Contact" ? "nav-link active" : "nav-link"
-            }
-          >
-            Contact
-          </a>
-        </li>
-        <li className="nav-item">
-          <a
-            href="#resume"
-            onClick={() => handlePageChange("Resume")}
-            // Ternary operater is checking if the current page is the the blog page, if it is on the blog it will make the nav-link active
-            className={
-              currentPage === "Resume" ? "nav-link active" : "nav-link"
-            }
-          >
-            Resume
-          </a>
-        </li>
-      </ul>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a
+              href="#AboutMe"
+              onClick={() => handlePageChange("AboutMe")}
+              // Ternary operater is checking if the current page is the About me page, if it is on the blog it will make the nav-link active
+              className={
+                currentPage === "AboutMe" ? "nav-link active" : "nav-link"
+              }
+            >
+              About Me
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#Portfolio"
+              onClick={() => handlePageChange("Portfolio")}
+              // Ternary operater is checking if the current page is the portfolio page, if it is on the blog it will make the nav-link active
+              className={
+                currentPage === "Portfolio" ? "nav-link active" : "nav-link"
+              }
+            >
+              Portfolio
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#contact"
+              // Ternary operater is checking if the current page is the the contact page, if it is on the blog it will make the nav-link active
+              onClick={() => handlePageChange("Contact")}
+              className={
+                currentPage === "Contact" ? "nav-link active" : "nav-link"
+              }
+            >
+              Contact
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#resume"
+              onClick={() => handlePageChange("Resume")}
+              // Ternary operater is checking if the current page is the the blog page, if it is on the blog it will make the nav-link active
+              className={
+                currentPage === "Resume" ? "nav-link active" : "nav-link"
+              }
+            >
+              Resume
+            </a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
