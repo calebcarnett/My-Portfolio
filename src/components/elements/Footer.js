714cda68a43f8cc1819875418c44ed11c5a0f409
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2023-03-13 11:55:43 -0400

    updated footer with links

--- src/components/elements/Footer.js
+++ src/components/elements/Footer.js
@@ -4,7 +4,37 @@ import "../../styles/styles/styles.css";
 export default function Footer() {
   return (
     <footer>
-      <p>Testing</p>
+      <footer className="bg-light text-center">
+        <div className="container p-4">
+          <section className="mb-4">
+            <a
+              className="btn btn-outline-dark btn-floating m-1"
+              href="https://www.linkedin.com/in/calebcarnett/"
+              role="button"
+            >
+              <i className="bi bi-linkedin"></i>
+            </a>
+
+            <a
+              className="btn btn-outline-dark btn-floating m-1"
+              href="https://github.com/calebcarnett"
+              role="button"
+            >
+              <i className="bi bi-github"></i>
+            </a>
+
+            <a
+              className="btn btn-outline-dark btn-floating m-1"
+              href="mailto:carnett.caleb@gmail.com"
+              role="button"
+            >
+              <i className="bi bi-envelope-fill"></i>
+            </a>
+          </section>
+        </div>
+
+        <div className="text-center p-3">© 2023 Copyright:</div>
+      </footer>
     </footer>
   );
 }
