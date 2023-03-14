bc01502b1bc6d58798c4b912665efbcea8cfbb4c
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2023-03-14 15:32:08 -0400

    added downloadable link for resume, finished ui changes

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -39,17 +39,12 @@ export default function AboutMe() {
 
           <div>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-              velit, lobortis ut magna varius, blandit rhoncus sem. Morbi
-              lacinia nisi ac dui fermentum, sed luctus urna tincidunt. Etiam ut
-              feugiat ex. Cras non risus mi. Curabitur mattis rutrum ipsum, ut
-              aliquet urna imperdiet ac. Sed nec nulla aliquam, bibendum odio
-              eget, vestibulum tortor. Cras rutrum ligula in tincidunt commodo.
-              Morbi sit amet mollis orci, in tristique ex. Donec nec ornare
-              elit. Donec blandit est sed risus feugiat porttitor. Vestibulum
-              molestie hendrerit massa non consequat. Vestibulum vitae lorem
-              tortor. In elementum ultricies tempus. Interdum et malesuada fames
-              ac ante ipsum primis in faucibus.
+              Completing a software development certification where I developed
+              a strong foundation in Javascript programming language. In my
+              previous position, I was known for my ability to solve problems
+              and execute tasks effectively and on time. Seeking a software
+              developer role where I can continue to apply my expertise through
+              hands-on projects.
             </p>
           </div>
         </section>
